7e5d9e1c2418b1e20c6bd2959819da68d307626c
Ankita Arya <ankitaarya1207@gmail.com>
2025-02-08 22:49:36 +0530

    java basics

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test{
+    int x=10;
+    public static void main(String[] args){
+        // System.out.println("Hello World!!");
+        int a=1;
+        System.out.println(Integer.toString(a));
+
+        String name="Ankita Arya";
+        System.out.println(name);
+
+        for(int i=0;i<8;i++){
+            System.out.println(i);
+        }
+    }
+}
